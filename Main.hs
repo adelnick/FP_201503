b9ce20ebf390e503895aeb8f2af418f8bd86f786
adelnick <nikolay@Q02521.quintiq.nl>
2015-03-17 17:52:15 +0100

    Before getting rid of Parser

--- Main.hs
+++ Main.hs
@@ -5,6 +5,9 @@ where
 import qualified Data.Map as M
 import qualified Data.Set as S
 import Data.Maybe (fromMaybe)
+import Control.Applicative ((<$>), (<*>), (<*), (*>))
+import System.IO 
+import Data.List.Split (splitOn)
 
 import           Text.Parsec
 import           Text.Parsec.String  (Parser)
@@ -13,24 +16,51 @@ type Region     = String
 type Bug        = String
 type Quantity   = String
 type RegionData = M.Map Bug Quantity
-type BugDB      = M.Map Region RegionData
+type AllBugs    = S.Set Bug
+type BRQData    = M.Map Region RegionData
+type BugDB      = (AllBugs, BRQData)
 
 type QtyRegions   = (Quantity, [Region])
 type BugInputData = (Bug, [QtyRegions])
 
-addBugRecord :: Bug -> Quantity -> Region -> BugDB -> BugDB
-addBugRecord r b q = M.alter (Just . M.insert b q . fromMaybe M.empty) r
+------------ Maintaining and querying DB      ------------
 
-addBugInfo :: BugInputData -> BugDB -> BugDB
-addBugInfo (b, xs) db = let ab   = addBugRecord b
+addBugRecord :: Bug -> Quantity -> Region -> BugDB -> BugDB
+addBugRecord b q r (a, rbq) = (S.insert b a, M.alter (Just . M.insert b q . fromMaybe M.empty) r rbq)
+
+addBugInfo :: BugDB -> BugInputData -> BugDB
+addBugInfo db (b, xs) = let ab   = addBugRecord b
                             abq  = map (\ (x,y) -> (ab x, y) ) xs
                             abqr = concat . map (\ (x,y) -> map x y ) $ abq
-                        in foldr ($) db abqr
+                        in foldr ($) (clearBugInfo db b) abqr
+
+clearBugInfo :: BugDB -> Bug -> BugDB
+clearBugInfo (a,db) b 
+  | b `S.member` a = (S.delete b a, M.map (M.delete b) db)
+  | otherwise    = (a,db)
 
 fillBugDB :: BugDB -> [BugInputData] -> BugDB
-fillBugDB db = foldr addBugInfo db 
+fillBugDB db = foldl addBugInfo db
 
------------- Parsing ------------
+getNrOfBugs :: RegionData -> Int
+getNrOfBugs = M.size
+
+getNrOfBugsStat :: BugDB -> (Region, Int)
+getNrOfBugsStat = map (\ (x,y) -> (x, getNrOfBugs y)). toList . snd
+
+------------ Exporting to CSV    ------------
+
+regionDataToCSV :: AllBugs -> RegionData -> String
+regionDataToCSV a rd = S.foldl (rdToCSV rd) "" a
+                       where rdToCSV rd s b = case (M.lookup b rd) of Just q   -> q ++ (';' : s)
+                                                                      Nothing  -> (';' : s)
+
+exportToCSV :: BugDB -> String
+exportToCSV (a, rbq) = unlines . map (\ (x, y) -> x ++ (';' : regionDataToCSV a y)) . M.toList $ rbq
+
+------------ Reading and Parsing ------------
+
+
 
 lineParser :: Parser String
 lineParser = manyTill anyChar (try newline)
@@ -39,24 +69,30 @@ bugParser :: Parser Bug
 bugParser = lineParser
 
 qtyRegionsParser :: Parser QtyRegions
-qtyRegionsParser = do { l <- lineParser
-                      ; let (q, rs) = span (/= ':') l
-                      ; let rs' = getRegions (tail rs)
-                      ; return (q, rs')
-                      }
+qtyRegionsParser = (,) <$> manyTill anyChar (string ": ") 
+                       <*> (getRegions <$> lineParser) -- it's easier to parse out a line and split it later, as the region name is not strictly defined
 
 getRegions :: String -> [Region]
-getRegions = gR []
-             where gR rs [] = rs
-                   gR rs xs = let (r,xs') = span (/= ',') xs
-                              in gR (tail r : rs) xs' -- Regions in reverse order but that's ok
+getRegions = splitOn ", "
 
+bugInfoParser :: Parser BugInputData
+bugInfoParser = (,) <$> bugParser <* spaces 
+                    <*> many (qtyRegionsParser <* spaces)
 
-{-
-parseBugsData :: String -> Parser BugInputData
-readBugsData = do { b <- lineParser <* spaces 
-                  ; 
--}
+parseBugInfo :: String -> Either ParseError BugInputData
+parseBugInfo = runParser bugInfoParser () ""
+
+readBugsData :: String -> IO (Maybe BugInputData)
+readBugsData f = do { handle <- openFile f ReadMode
+                    ; hSetEncoding handle utf8_bom
+                    ; fc <- hGetContents handle
+                    ; let ebi = parseBugInfo fc
+                    ; return (case ebi of (Left err) -> Nothing
+                                          (Right bi) -> Just bi)
+                    }
+
+---------------------------------------------
+
 main :: IO ()
 main = do { return ();
           }
