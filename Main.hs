43fe709484bb7e463dd8bcc547aa3d428fa6f417
adelnick <nikolay@Q02521.quintiq.nl>
2015-03-18 16:35:07 +0100

    Small bug in extinction risk

--- Main.hs
+++ Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
 
 module Main
 
@@ -29,7 +29,13 @@ type BugInputData = (Bug, [QtyRegions])
 type States      = M.Map Region Double
 type Frequencies = M.Map Quantity Double
 
------------- Maintaining and querying DB      ------------
+type BugExtinctionRisk = (Bug,Double)
+
+type Dataset = (BugDB, States, Frequencies)
+
+------------ Maintaining and querying Dataset      ------------
+
+createDS = ((S.empty, M.empty), M.empty, M.empty)
 
 addBugRecord :: Bug -> Quantity -> Region -> BugDB -> BugDB
 addBugRecord b q r (a, rbq) = (S.insert b a, M.alter (Just . M.insert b q . fromMaybe M.empty) r rbq)
@@ -54,13 +60,29 @@ getNrOfBugs = M.size
 getNrOfBugsStat :: BugDB -> [(Region, Int)]
 getNrOfBugsStat = map (\ (x,y) -> (x, getNrOfBugs y)) . M.toList . snd
 
------------- Exporting to CSV    ------------
+calcExtinctionRiskPerRegion :: Double -> Bug -> Frequencies -> RegionData -> Double
+calcExtinctionRiskPerRegion rCoef b f rd = let mbQ = M.lookup b rd
+                                           in case mbQ of (Just q) -> (f M.! q) * rCoef
+
+
+calcBugExtinctionRisk :: BRQData -> States -> Frequencies -> Bug -> BugExtinctionRisk
+calcBugExtinctionRisk brq s f b = (b, M.foldWithKey getRisk 0.0 brq)
+                                 where getRisk r rd extCoef = extCoef + (s M.! r) * (fCoef rd)
+                                       fCoef rd = let mbQ = M.lookup b rd
+                                                  in case mbQ of (Just q) -> (f M.! (rd M.! b))
+                                                                 Nothing  -> 0.0
+ 
+
+calcExtinctionRisk :: Dataset -> [BugExtinctionRisk]
+calcExtinctionRisk ((a,brq),s,f) = map (calcBugExtinctionRisk brq s f) $ S.toList a
+
+------------ Export             ------------
 allBugsToCSV :: AllBugs -> String
 allBugsToCSV = (';' : ) . intercalate ";" . S.toList
 
 regionDataToCSV :: AllBugs -> RegionData -> String
-regionDataToCSV a rd = S.foldl (rdToCSV rd) "" a
-                       where rdToCSV rd s b = case (M.lookup b rd) of Just q   -> q ++ (';' : s)
+regionDataToCSV a rd = S.foldr (rdToCSV rd) "" a
+                       where rdToCSV rd b s = case (M.lookup b rd) of Just q   -> q ++ (';' : s)
                                                                       Nothing  -> (';' : s)
 
 exportToCSV :: BugDB -> String
@@ -69,30 +91,28 @@ exportToCSV (a, rbq) = unlines . (allBugsToCSV a :) . map (\ (x, y) -> x ++ (';'
 printUTFString :: String -> IO ()
 printUTFString = T.putStr . T.pack
 
+showNrOfBugsStat :: [(Region, Int)] -> String
+showNrOfBugsStat = unlines . map (\ (x,y) -> show x ++ (':' : ' ' : show y))
+
+showExtinctionRisk :: BugExtinctionRisk -> String
+showExtinctionRisk (b,x) = b ++ (';' : ' ' : show x)
+
 ------------ Reading and Parsing ------------
 
 qtyRegionsParser :: String -> QtyRegions
 qtyRegionsParser s = let (q,rs) = span (/= ':') s
-                     in (q, splitOn ", " (tail rs))
+                     in (q, splitOn ", " (tail .tail $ rs))
 
 parseBugInfo :: String -> BugInputData                               -- No error handling, it's tedious
 parseBugInfo xs = let (b : _ : ys) = lines xs
                   in (b, map qtyRegionsParser ys)
 
-readBugsData :: String -> IO (BugInputData)
-readBugsData f = do { t <- T.readFile f
+readBugData :: String -> IO (BugInputData)
+readBugData f = do { t <- T.readFile f
                     ; return $ parseBugInfo . T.unpack $ t
                     }
-{-
-readBugsData f = do { handle <- openFile f ReadMode
-                    ; hSetEncoding handle utf8_bom
-                    ; fc <- hGetContents handle
-                    ; return $ parseBugInfo fc
-                    }
--}
-
 parseStates :: String -> States
-parseStates s = foldl insState M.empty (map convert (lines s))
+parseStates s = foldl insState M.empty (map convert (filter (not . null) (lines s)))
                 where convert = (\(x,y) -> (tail y, read x :: Double)) . span (/= ' ')
                       insState m (s,v) = M.insert s v m
 
@@ -101,18 +121,66 @@ readStates f = do { t <- T.readFile f
                   ; return $ parseStates . T.unpack $ t
                   }
 
---parseFrequencies :: String -> Frequencies
---parseFrequencies = map (insert . flip . span (== ' ')) . lines
+parseFrequencies :: String -> Frequencies
+parseFrequencies s = foldl insFrequency M.empty (map convert (filter (not . null) (lines s)))
+                where convert = (\(x,y) -> (tail y, read x :: Double)) . span (/= ' ')
+                      insFrequency m (s,v) = M.insert s v m
+
+readFrequencies :: String -> IO (States)
+readFrequencies f = do { t <- T.readFile f
+                  ; return $ parseFrequencies . T.unpack $ t
+                  }
 
 ---------------------------------------------
 
+menuLoad :: BugDB -> IO (BugDB)
+menuLoad db = do { print "Input file name"
+                 ; f <- getLine
+                 ; b <- readBugData f
+                 ; return $ addBugInfo db b
+                 }
+
+menuExport :: BugDB -> IO ()
+menuExport db = do { print "Input file name"
+                   ; f <- getLine
+                   ; T.writeFile f (T.pack . exportToCSV $ db)
+--                   ; printUTFString $ exportToCSV db
+                   }
+
+menuNrOfBugs :: BugDB -> IO ()
+menuNrOfBugs = printUTFString . showNrOfBugsStat . getNrOfBugsStat
+
+menuLoadStatesAndFrequencies :: IO ((States,Frequencies))
+menuLoadStatesAndFrequencies = do { print "Input states file name"
+                                  ; sf <- getLine
+                                  ; s <- readStates sf
+                                  ; print "Input frequencies file name"
+                                  ; ff <- getLine
+                                  ; f <- readFrequencies ff
+                                  ; return (s,f)
+                                  }
+
+menuExtinctionRisk :: Dataset -> IO ()
+menuExtinctionRisk = printUTFString . unlines . map showExtinctionRisk . calcExtinctionRisk
+
+userMenu :: Dataset -> IO ()
+userMenu ds@(db,s,f) = do { print "1 - Load bug info from file"
+                          ; print "2 - Export to CSV"
+                          ; print "3 - Display number of bugs per region"
+                          ; print "4 - Load state and frequency info"
+                          ; print "5 - Display extinction risk"
+                          ; print "0 - Exit"
+                          ; c <- getLine
+                          ; case c of "1" -> (,s,f) <$> menuLoad db >>= userMenu
+                                      "2" -> menuExport db >> userMenu ds
+                                      "3" -> menuNrOfBugs db >> userMenu ds
+                                      "4" -> uncurry (db,,) <$> menuLoadStatesAndFrequencies >>= userMenu
+                                      "5" -> menuExtinctionRisk ds >> userMenu ds
+                                      "0" -> return ()
+                 }
+
+
 main :: IO ()
-main = do { c <- readBugsData "D:/Git/FP_201503/data/Aurata.dat"
-          ; let db = addBugInfo (S.empty, M.empty) c
-          ; let csv = exportToCSV db
-          ; print csv
-          ; printUTFString csv
-          ; s <- readStates "D:/Git/FP_201503/data/States.txt"
-          ; printUTFString $ show s
+main = do { userMenu createDS
           }
 	  
